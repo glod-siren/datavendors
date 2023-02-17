cc8a9e56443559cf15377f94de642819f766a83d
Phil Glod <phil.glod@siren.io>
2023-02-17 01:17:05 -0500

    ok

--- consolidatedscreeninglist/src/index.ts
+++ consolidatedscreeninglist/src/index.ts
@@ -2,4 +2,5 @@ import { Joi, registerServices } from '@sirensolutions/web-service-interface';
 import Search from './Search';
 import Sources from './Sources';
 const configSchema = {subscription_key: Joi.string().default('change-me')};
+// ok
 export = registerServices('consolidatedscreeninglist', [Search, Sources], configSchema);
